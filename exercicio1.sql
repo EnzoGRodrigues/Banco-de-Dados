2bdad444075745d54fa76f84178da6c5ae7e5f9a
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-03-09 13:14:52 -0300

    Update exercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -1,9 +1,9 @@
 create table area(
-    id_area number constraint pk_area primary key,
+    ID_area number constraint pk_area primary key,
     nome varchar2(20) constraint nomeArea_area not null
 );
 create table curso(
-    id_curso number constraint pk_curso primary key,
+    ID_curso number constraint pk_curso primary key,
     nome varchar2(20) constraint nomeCurso_curso not null,
     semestre number constraint semestreCurso_curso not null,
     turno varchar2(20) constraint turnoCurso_curso not null,
@@ -11,7 +11,7 @@ create table curso(
 );
 
 create table disciplina(
-    id_disciplina number constraint pk_disciplina primary key,
+    ID_disciplina number constraint pk_disciplina primary key,
     codigo number constraint codigoDisciplina_disciplina not null,
     nome varchar2(40) constraint nomeDisciplina_disciplina not null,
     creditos varchar2(40) constraint creditosDisciplina_disciplina not null,
@@ -19,16 +19,16 @@ create table disciplina(
 );
 
 create table aluno(
-    id_aluno number constraint pk_aluno primary key,
+    ID_aluno number constraint pk_aluno primary key,
     nome varchar2(40) constraint nomeAluno_aluno not null,
     data varchar2(40) constraint dataAluno_aluno not null
 );
 
 create table professor(
-    id_professor number constraint pk_professor primary key,
+    ID_professor number constraint pk_professor primary key,
     nome varchar2(40) constraint nomeProfessor_professor not null,
     horas varchar2(40) constraint horasProfessor_professor not null,
-    id_area number constraint nArea_id_area not null
+    ID_area number constraint mArea_ID_area not null
 );
 
 create table turma(
@@ -36,23 +36,32 @@ create table turma(
     semestre varchar2(40) constraint semestreTurma_turma not null,
     turno varchar2(40) constraint turnoTurma_turma not null,
     maximo varchar2(40) constraint maximoaluno_turma not null,
-    id_disciplina number constraint nDisciplina_id_disciplina not null,
-    id_professor number constraint nProfessor_id_professor not null
+    ID_disciplina number constraint nDisciplina_id_disciplina not null,
+    ID_professor number constraint nProfessor_id_professor not null
 );
 create table turmaAluno(
-    id_turma number,
-    id_aluno number,
+    ID_turma number,
+    ID_aluno number,
     mediafinal number constraint mediafinal_turma not null,
     constraint pk_turmaAluno primary key (id_aluno, id_turma)
 );
 
 create table alunoCurso(
-    id_aluno number,
-    id_curso number,
+    ID_aluno number,
+    ID_curso number,
     data number constraint datainicial_aluno not null,
     constraint pk_alunoCurso primary key (id_aluno, id_curso)
 );
 
+alter table curso add
+    (constraint fk_Area_ID_area foreign key (ID_area) references area(ID_area));
+alter table disciplina add
+    (constraint fk_Curso_ID_curso foreign key (id_curso)references curso(ID_curso));
+alter table professor add
+    (constraint fk_pArea_ID_area foreign key (ID_area) references area(ID_area));
+alter table turma add
+    (constraint fk_Disciplina_ID_disciplina foreign key (ID_disciplina) references disciplina(ID_disciplina),
+    constraint fk_Professor_ID_professor foreign key (ID_professor) references professor(ID_professor));
 
 
 drop table area;
