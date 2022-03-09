271a74303bd1c9a05d98bd46897a0676b8cecadd
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-03-09 17:02:45 -0300

    Update exercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -1,21 +1,19 @@
 create table area(
     ID_area number constraint pk_area primary key,
-    nome varchar2(20) constraint nomeArea_area not null
+    nome varchar2(40) constraint nomeArea_area not null
 );
 create table curso(
     ID_curso number constraint pk_curso primary key,
-    nome varchar2(20) constraint nomeCurso_curso not null,
-    semestre number constraint semestreCurso_curso not null,
-    turno varchar2(20) constraint turnoCurso_curso not null,
-    id_area number constraint nArea_id_area not null
+    nome varchar2(40) constraint nomeCurso_curso not null,
+    semestre varchar2(40) constraint semestreCurso_curso not null,
+    turno varchar2(40) constraint turnoCurso_curso not null
 );
 
 create table disciplina(
     ID_disciplina number constraint pk_disciplina primary key,
     codigo number constraint codigoDisciplina_disciplina not null,
     nome varchar2(40) constraint nomeDisciplina_disciplina not null,
-    creditos varchar2(40) constraint creditosDisciplina_disciplina not null,
-    id_curso number constraint nCurso_id_curso not null
+    creditos varchar2(40) constraint creditosDisciplina_disciplina not null
 );
 
 create table aluno(
@@ -27,17 +25,14 @@ create table aluno(
 create table professor(
     ID_professor number constraint pk_professor primary key,
     nome varchar2(40) constraint nomeProfessor_professor not null,
-    horas varchar2(40) constraint horasProfessor_professor not null,
-    ID_area number constraint mArea_ID_area not null
+    horas varchar2(40) constraint horasProfessor_professor not null
 );
 
 create table turma(
-    id_turma number constraint pk_turma primary key,
+    iD_turma number constraint pk_turma primary key,
     semestre varchar2(40) constraint semestreTurma_turma not null,
     turno varchar2(40) constraint turnoTurma_turma not null,
-    maximo varchar2(40) constraint maximoaluno_turma not null,
-    ID_disciplina number constraint nDisciplina_id_disciplina not null,
-    ID_professor number constraint nProfessor_id_professor not null
+    maximo varchar2(40) constraint maximoaluno_turma not null
 );
 create table turmaAluno(
     ID_turma number,
@@ -54,65 +49,94 @@ create table alunoCurso(
 );
 
 alter table curso add
-    (constraint fk_Area_ID_area foreign key (ID_area) references area(ID_area));
+    (constraint fk_cArea_ID_area foreign key (ID_curso) references area(ID_area));
 alter table disciplina add
-    (constraint fk_Curso_ID_curso foreign key (id_curso)references curso(ID_curso));
+    (constraint fk_Curso_ID_curso foreign key (ID_disciplina)references curso(ID_curso));
 alter table professor add
-    (constraint fk_pArea_ID_area foreign key (ID_area) references area(ID_area));
+    (constraint fk_pArea_ID_area foreign key (ID_professor) references area(ID_area));
 alter table turma add
-    (constraint fk_Disciplina_ID_disciplina foreign key (ID_disciplina) references disciplina(ID_disciplina),
-    constraint fk_Professor_ID_professor foreign key (ID_professor) references professor(ID_professor));
+    (constraint fk_Disciplina_ID_disciplina foreign key (ID_turma) references disciplina(ID_disciplina),
+    constraint fk_Professor_ID_professor foreign key (ID_turma) references professor(ID_professor));
 
 
 create sequence a_area
 increment by 1
-start with 10
-maxvalue 1000
+start with 1
+maxvalue 10
 order
 nocache;
 
 create sequence b_curso
 increment by 1
 start with 20
-maxvalue 2000
+maxvalue 29
 order
 nocache;
 
 create sequence c_disciplina
 increment by 1
-start with 30
-maxvalue 3000
+start with 300
+maxvalue 399
 order
 nocache;
 
 create sequence d_aluno
 increment by 1
-start with 40
-maxvalue 4000
+start with 4000
+maxvalue 4999
 order
 nocache;
 
 create sequence e_professor
 increment by 1
-start with 50
-maxvalue 5000
+start with 50000
+maxvalue 59999
 order
 nocache;
 
 create sequence f_turma
 increment by 1
-start with 60
-maxvalue 6000
+start with 600000
+maxvalue 699999
 order
 nocache;
 
+insert into area(ID_area, nome) VALUES ('1','Matematica');
+insert into area(ID_area, nome) VALUES ('2','Informatica');
+insert into area(ID_area, nome) VALUES ('3','Saude');
 
+insert into curso(ID_curso, nome, semestre, turno) VALUES ('20', 'Matematica','2º','manha');
+insert into curso(ID_curso, nome, semestre, turno) VALUES ('21', 'Engenharia de Software','4º','tarde');
+insert into curso(ID_curso, nome, semestre, turno) VALUES ('22', 'Enfermagem','7º','noite');
 
-drop table area;
-drop table curso;
-drop table disciplina;
-drop table aluno;
-drop table professor;
-drop table turma;
-drop table turmaAluno;
-drop table alunoCurso;
+insert into disciplina(id_disciplina, codigo, nome, creditos) VALUES ('300','5','Algoritmo','10 cr');
+insert into disciplina(id_disciplina, codigo, nome, creditos) VALUES ('301','1','Estrutura de Dados','20 cr');
+insert into disciplina(id_disciplina, codigo, nome, creditos) VALUES ('302','8','Anatomia','15 cr');
+
+insert into aluno(id_aluno, nome, data) VALUES ('4000','Enzo','07/04/01');
+insert into aluno(id_aluno, nome, data) VALUES ('4001','Lucas','10/12/99');
+insert into aluno(id_aluno, nome, data) VALUES ('4002','Carol','23/03/96');
+
+insert into professor(id_professor, nome, horas) values ('50000','Luis Soares','66h');
+insert into professor(id_professor, nome, horas) values ('50001','Paula Vencato','33h');
+insert into professor(id_professor, nome, horas) values ('50002','Cesar Castro','120h');
+
+insert into turma(iD_turma, semestre, turno, maximo) VALUES ('600000','3º','manha','30 alunos');
+insert into turma(iD_turma, semestre, turno, maximo) VALUES ('600001','3º','noite','15 alunos');
+insert into turma(iD_turma, semestre, turno, maximo) VALUES ('600002','3º','manha','45 alunos');
+
+drop table area cascade constraints ;
+drop table curso cascade constraints ;
+drop table disciplina cascade constraints ;
+drop table aluno cascade constraints ;
+drop table professor cascade constraints ;
+drop table turma cascade constraints ;
+drop table turmaAluno cascade constraints ;
+drop table alunoCurso cascade constraints ;
+
+drop sequence a_area;
+drop sequence b_curso;
+drop sequence c_disciplina;
+drop sequence d_aluno;
+drop sequence e_professor;
+drop sequence f_turma;
